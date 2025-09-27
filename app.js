e640b588c99f65f51ebdda014b56f484b03dd444
Waheed-Labib <labibwahid28@gmail.com>
2025-09-27 18:08:41 +0600

    updated cors in app.js

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+const allowedOrigins = ['https://scriptures-react-project.vercel.app'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);         // allow non-web (curl, mobile)
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE']
+};
+app.use(cors(corsOptions));
+
 
 app.use(express.json({ limit: '16kb' }));
 
